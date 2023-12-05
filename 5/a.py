d54e7e5c4890fb3a69811c000cf4ae58af047105
Acrylic125 <acrylic125email@gmail.com>
2023-12-05 18:51:29 +0800

    a

--- 5/a.py
+++ 5/a.py
@@ -2,24 +2,33 @@ import re
 from typing import List
 
 with open("data.txt", "r+") as f:
-    l = f.readlines()
-    sum = 0
-    for line in l:
-        line = line.replace("\n", "")
-        card, nums = line.split(": ")
-        winning, guess = nums.split(" | ")
-        winning_nums = re.findall(r'\d+', winning)
-        guess_nums = re.findall(r'\d+', guess)
+    l = f.read()
+    _maps = l.split("\n\n")
+    seed = _maps[0]
+    maps = _maps[1:]
 
-        score = 0
-        for num in guess_nums:
-            if num in winning_nums:
-                if score == 0:
-                    score = 1
+    values = list(map(lambda s: int(s), re.findall(r"\d+", seed)))
+    for m in maps:
+        ms = m.split("\n")
+        print(ms[0])
+        map_rules = ms[1:]
+
+        mapped_indices = []
+        for rule in map_rules:
+            dest_start, src_start, r = map(lambda s: int(s), rule.split(" "))
+            new_values = []
+
+            i = 0
+            for v in values:
+                if i not in mapped_indices and (v >= src_start and v < src_start + r):
+                    new_values.append(v - src_start + dest_start)
+                    mapped_indices.append(i)
                 else:
-                    score = score * 2
-        sum = sum + score
-        
-        # print(line)
-        # print(winning_nums, guess_nums)
-    print(sum)
+                    new_values.append(v)
+                i = i + 1
+            print([dest_start, src_start, r], values, new_values)
+            values = new_values
+    
+    print(values)
+    print(min(values))
+    # print(maps)
